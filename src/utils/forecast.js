adc416a43240d8f1fded6ba736ce77dc4e10a6ac
unknown <mandidisha@gmail.com>
2022-02-09 14:43:33 +0100

    new data for weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,8 @@
 const request = require("request")
+
+
+
+
 const forecast= (latitude, longitude, callback) =>{
     const url ='http://api.weatherstack.com/current?access_key=ccde9bbb71f9617697a6341fc3808f56&query='+latitude +','+ longitude +'&units=f'
 
@@ -8,7 +12,7 @@ const forecast= (latitude, longitude, callback) =>{
         }else if (body.error){
             callback('unable to find cordinates Try another search', undefined)
         }else{
-            callback(undefined ,body.current.weather_descriptions[0] + '. It currently is '+body.current.temperature + ' degrees out. It feels like '+ body.current.feelslike+ ' degrees out')
+            callback(undefined ,body.current.weather_descriptions[0] + '. It currently is '+body.current.temperature + ' degrees out. It feels like '+ body.current.feelslike+ ' degrees out. Humidity is '+ body.current.humidity)
         }
         
     })
